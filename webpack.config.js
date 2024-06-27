1c000a04fa58746714523884a29f9d2adac705bc
Shantanu <dekateshantanu015@gmail.com>
2024-06-27 11:23:56 +0530

    Added initial HTML layout

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js", // Entry point for your application
   output: {
     filename: "main.js", // Output file name
-    path: path.resolve(__dirname, "dist"), // Output directory
+    path: path.resolve(__dirname, "dist/js"), // Output directory
   },
   mode: "production", // Set the mode to development or production
 };
